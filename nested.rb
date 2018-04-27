6eb9aab7f0cd888bcfd8e4d9639d2c7dc373118b
Suchitra Naik <susanaik@gmail.com>
2018-04-27 13:58:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,11 +84,11 @@ def adding_matz
         }
      }
 
-programmer_hash.unshift(
+programmer_hash.first = {
 :yukihiro_matsumoto => {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
- })
+ }
 
     
 end
